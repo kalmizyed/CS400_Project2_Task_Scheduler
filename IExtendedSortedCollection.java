3568444a9c8ec0a7f52bcebfe03ffa2da6a1ae24
Jack Blake <jhblake2@wisc.edu>
2022-03-09 08:26:01 -0600

    Made the IExtendedSortedCollection Interface more descriptive and specify what type of constructor I will implement it with

--- IExtendedSortedCollection.java
+++ IExtendedSortedCollection.java
@@ -1,8 +1,21 @@
+// --== CS400 Project Two File Header ==--
+// Name: Jack Blake
+// CSL Username: jblake
+// Email: jhblake2@wisc.edu
+// Lecture #: 004 @4:00pm
+// Notes to Grader: 
+
+/**
+ * Interface representing an iterable sorted collection which you can insert elements into and remove elements from.
+ */
 public interface IExtendedSortedCollection<T extends Comparable<T>> extends SortedCollectionInterface<T> {
+
+    //The Red Black Tree I implement this with will use a default, no argument constructor.
+
     /**
      * Removes the node with the data for which compareTo() returns 0.
      * @param data the object to be removed
      * @return the object being removed, or null if nothing was removed
      */
-    T remove(T data);
+    public T remove(T data);
 }
