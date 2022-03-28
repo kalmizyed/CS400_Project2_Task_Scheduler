f4abdebc65d14effe46d140d5eaeb1f00b85418c
Joseph Cai <jbcai@wisc.edu>
2022-03-28 15:13:35 -0500

    Finished basic functionality of backend

---
+++ TaskPlaceholder.java
@@ -0,0 +1,25 @@
+import java.util.Date;
+
+public class TaskPlaceholder implements ITask{
+    private String name;
+    private Date date;
+    public TaskPlaceholder(String name, Date date){
+        this.name = name;
+        this.date = date;
+    }
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public int compareTo(ITask other) {
+        return date.compareTo(other.getDate());
+    }
+    
+}
---
+++ TreeFileHandlerPlaceholder.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class TreeFileHandlerPlaceholder implements ITreeFileHandler{
+
+    @Override
+    public IExtendedSortedCollection<ITask> getTreeFromFile(File f) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean writeTreeToFile(File f, IExtendedSortedCollection<ITask> tree) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
