8089bf3c3eea1e33b47c386d0543c3fd67751a77
Joseph Cai <jbcai@wisc.edu>
2022-04-04 18:42:45 -0500

    Added 4 new tests

--- BackendDeveloperTests.java
+++ BackendDeveloperTests.java
@@ -9,10 +9,14 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.beans.Transient;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.zip.DataFormatException;
 
 public class BackendDeveloperTests {
     /**
@@ -90,13 +94,119 @@ public class BackendDeveloperTests {
     }
 
     /**
-     * Tests XML load and save functionality. Since that's mostly DataWrangler's job, I just test to see if the file exists
+     * Tests XML save functionality. This just tests to see if the file exists
      */
     @Test
-    void testLoadSave(){
+    void testSaveTree(){
         TaskSchedulerBackend backend = new TaskSchedulerBackend();
+        backend.addTask("03/03/2022-12:12", "Test");
         backend.saveTree();
-        backend.loadTree();
-        assertEquals(backend.getAllTasks().size(), 4);
+        File xmlFile = new File("tasks.xml");
+        assertTrue(xmlFile.exists());
+    }
+
+    /******************************* 
+        START OF NEW TESTS
+    ****************************/
+    /*
+     * Tests XML load and save functionality, with only one task
+     */
+    @Test
+    void testLoadAndSaveTree() throws FileNotFoundException, DataFormatException{
+        //Original backend
+        TaskSchedulerBackend backend = new TaskSchedulerBackend();
+        backend.addTask("03/03/2022-12:12", "Test");
+        backend.saveTree(); //Save to file
+        File xmlFile = new File("tasks.xml");
+
+        //New backend
+        TaskSchedulerBackend backend2 = new TaskSchedulerBackend();
+        backend2.loadTree(); //Load from file
+        ITask task = backend2.getAllTasks().get(0);
+        assertEquals(task.getName(), "Test"); //Task with same name
+        assertEquals(task.getDate().toString(), "Thu Mar 03 12:12:00 CST 2022"); //Same date
+    }
+    
+    /**
+     * Tests XML load and save functionality, with many tasks
+     */
+    @Test
+    void testLoadAndSaveTree2() throws FileNotFoundException, DataFormatException{
+        //Original backend
+        TaskSchedulerBackend backend = new TaskSchedulerBackend();
+        //Add tasks
+        backend.addTask("03/05/2022-12:12", "Test5");
+        backend.addTask("03/01/2022-12:12", "Test1");
+        backend.addTask("03/04/2022-12:12", "Test4");
+        backend.addTask("03/03/2022-12:12", "Test3");
+        backend.addTask("03/02/2022-12:12", "Test2");
+        backend.saveTree(); //Save to file
+        File xmlFile = new File("tasks.xml");
+
+        //New backend
+        TaskSchedulerBackend backend2 = new TaskSchedulerBackend();
+        backend2.loadTree(); //Load from file
+        //Get all tasks
+        ITask task1 = backend2.getAllTasks().get(0);
+        ITask task2 = backend2.getAllTasks().get(1);
+        ITask task3 = backend2.getAllTasks().get(2);
+        ITask task4 = backend2.getAllTasks().get(3);
+        ITask task5 = backend2.getAllTasks().get(4);
+        //Check same name and same date
+        assertEquals(task1.getName(), "Test1");
+        assertEquals(task1.getDate().toString(), "Tue Mar 01 12:12:00 CST 2022");
+        assertEquals(task2.getName(), "Test2");
+        assertEquals(task2.getDate().toString(), "Wed Mar 02 12:12:00 CST 2022");
+        assertEquals(task3.getName(), "Test3");
+        assertEquals(task3.getDate().toString(), "Thu Mar 03 12:12:00 CST 2022");
+        assertEquals(task4.getName(), "Test4");
+        assertEquals(task4.getDate().toString(), "Fri Mar 04 12:12:00 CST 2022");
+        assertEquals(task5.getName(), "Test5");
+        assertEquals(task5.getDate().toString(), "Sat Mar 05 12:12:00 CST 2022");
+    }
+
+    /**************************************
+        Tests for Algorithm Engineer
+    **************************************/
+    /**
+     * Tests redBlackTree insert operations
+     */
+    @Test
+    void testInsert(){
+        RedBlackTree<Integer> tree = new RedBlackTree<Integer>();
+        tree.insert(1);
+        tree.insert(5);
+        tree.insert(4);
+        tree.insert(3);
+        tree.insert(2);
+        assertTrue(tree.contains(1));
+        assertTrue(tree.contains(2));
+        assertTrue(tree.contains(3));
+        assertTrue(tree.contains(4));
+        assertTrue(tree.contains(5));
+        assertTrue(!tree.contains(0));
+        assertTrue(!tree.contains(6));
+    }
+    /**
+     * Tests redBlackTree remove operations
+     */
+    @Test
+    void testRemove(){
+        RedBlackTree<Integer> tree = new RedBlackTree<Integer>();
+        tree.insert(1);
+        tree.insert(5);
+        tree.insert(4);
+        tree.insert(3);
+        tree.insert(2);
+        tree.remove(1);
+        assertTrue(!tree.contains(1));
+        tree.remove(2);
+        assertTrue(!tree.contains(2));
+        tree.remove(3);
+        assertTrue(!tree.contains(3));
+        tree.remove(4);
+        assertTrue(!tree.contains(4));
+        tree.remove(5);
+        assertTrue(tree.isEmpty());
     }
 }
