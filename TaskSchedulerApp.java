a2a65bc61c1ed9c0180a63cee986496283f47d05
Joseph Cai <jbcai@wisc.edu>
2022-04-03 21:05:06 -0500

    Got app working

--- TaskSchedulerApp.java
+++ TaskSchedulerApp.java
@@ -3,8 +3,7 @@
  */
 public class TaskSchedulerApp {
     public static void main(String[] args) {
-        // TODO replace null with TaskSchedulerFrontend when code is merged
-        ITaskSchedulerFrontend frontend = null;
+        ITaskSchedulerFrontend frontend = new TaskScheduleUI(new TaskSchedulerBackend());
         frontend.runCommandLoop();
     }
 }
